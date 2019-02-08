af2c0100314c4199295870f7bd015f0b230cbca3
fox <fox@fox.fox>
2019-02-08 21:31:49 +0300

    default map script

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Map from './Map';
 
 class Home extends Component {
 
 	render() {
 		return(
 			<div>
-				Home
+				<Map
+				google={this.props.google}
+				center={{lat: 18.5204, lng: 73.8567}}
+				height='300px'
+				zoom={15}
+				/>
 			</div>
 		);
 	}
